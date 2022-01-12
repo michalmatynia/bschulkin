3331e8be1e230694c7db038658c8c5d0ec0fba66
Michał Matynia <mmatynia@gmail.com>
2022-01-12 09:13:47 +0100

    basis for app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
+import { shallow, ShallowWrapper } from 'enzyme';
+import { findByTestAttr } from './test/testUtils';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+/**
+ * Setup function for App Component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App/>)
+}
+test('renders without error', async () => {
+
+const wrapper = setup()
+const appComponent = await findByTestAttr(wrapper, 'component-app')
+expect(appComponent).toHaveLength(1)
 });
